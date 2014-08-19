e10bbbaf575519e012fab2d7b48cb783bf76a064
Christopher Orr <chris@orr.me.uk>
2014-08-19 16:06:27 +0200

    Work around GoogleJsonResponseException weirdness with HTTP 401 responses.

--- src/main/java/org/jenkinsci/plugins/googleplayandroidpublisher/PublisherApiException.java
+++ src/main/java/org/jenkinsci/plugins/googleplayandroidpublisher/PublisherApiException.java
@@ -1,5 +1,6 @@
 package org.jenkinsci.plugins.googleplayandroidpublisher;
 
+import com.google.api.client.googleapis.json.GoogleJsonError;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 
 import java.io.IOException;
@@ -14,8 +15,17 @@ public class PublisherApiException extends UploadException {
         // We need to extract the meaningful part of the exception here because GJRE isn't Serializable,
         // and so cannot be passed back from the build slave to Jenkins master without losing information
         if (cause instanceof GoogleJsonResponseException) {
-            // TODO: It could be nice to serialise more of this structure, e.g. error reason codes
-            this.detailsMessage = ((GoogleJsonResponseException) cause).getDetails().getMessage();
+            GoogleJsonError details = ((GoogleJsonResponseException) cause).getDetails();
+            if (details == null) {
+                // The details don't seem to be populated in the case of 401 errors:
+                // https://code.google.com/p/google-api-java-client/issues/detail?id=898
+                if (((GoogleJsonResponseException) cause).getStatusCode() == 401) {
+                    this.detailsMessage = "The API credentials provided do not have permission to apply these changes";
+                }
+            } else {
+                // TODO: It could be nice to serialise more of this structure, e.g. error reason codes
+                this.detailsMessage = details.getMessage();
+            }
         }
     }
 
